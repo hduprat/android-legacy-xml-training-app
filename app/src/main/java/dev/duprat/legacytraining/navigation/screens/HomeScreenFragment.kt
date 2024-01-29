d15ee04da50e9dd0660a2a36b75f13748a20da34
hduprat <hugod@bam.tech>
2024-01-29 10:39:56 +0100

    First commit

---
+++ app/src/main/java/dev/duprat/legacytraining/navigation/screens/HomeScreenFragment.kt
@@ -0,0 +1,19 @@
+package dev.duprat.legacytraining.navigation.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dev.duprat.legacytraining.R
+
+class HomeScreenFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        inflater.inflate(R.layout.home_screen, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
