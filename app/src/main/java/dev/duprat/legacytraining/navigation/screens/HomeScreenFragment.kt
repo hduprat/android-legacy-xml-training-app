16441fa13842e95fff15498317da535a600fdc2b
hduprat <hugod@bam.tech>
2024-01-30 12:07:41 +0100

    refactor(BeerCard): convert from Fragment to custom layout

--- app/src/main/java/dev/duprat/legacytraining/navigation/screens/HomeScreenFragment.kt
+++ app/src/main/java/dev/duprat/legacytraining/navigation/screens/HomeScreenFragment.kt
@@ -5,21 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import dev.duprat.legacytraining.R
 import dev.duprat.legacytraining.beer.model.Beer
-import dev.duprat.legacytraining.beer.ui.BeerCardFragment
 import dev.duprat.legacytraining.databinding.HomeScreenBinding
 
 class HomeScreenFragment : Fragment() {
+    private lateinit var binding: HomeScreenBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        return HomeScreenBinding.inflate(inflater, container, false).root
+        binding = HomeScreenBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val beerCard = BeerCardFragment.newInstance(
+        binding.beerCard.setBeer(
             Beer(
                 name = "Punk IPA",
                 tagline = "Post Modern Classic. Spiky. Tropical. Hoppy.",
@@ -29,7 +29,5 @@ class HomeScreenFragment : Fragment() {
                 description = "Our scene-stealing flagship is an India Pale Ale that has become a byword for craft beer rebellion; synonymous with the insurgency against mass-produced, lowest common denominator beer. Punk IPA charges the barricades to fly its colours from the ramparts – full-on, full-flavour; at full-throttle."
             )
         )
-
-        childFragmentManager.beginTransaction().replace(R.id.beer_card_fragment, beerCard).commit()
     }
 }
