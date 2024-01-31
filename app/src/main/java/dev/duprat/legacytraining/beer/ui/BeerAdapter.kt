6fa933b470bb8d227c68e22816d5e87206bddb3f
hduprat <hugod@bam.tech>
2024-01-31 12:04:07 +0100

    feat(BeerAdapter): extend card width to the max

--- app/src/main/java/dev/duprat/legacytraining/beer/ui/BeerAdapter.kt
+++ app/src/main/java/dev/duprat/legacytraining/beer/ui/BeerAdapter.kt
@@ -5,8 +5,16 @@ import androidx.recyclerview.widget.RecyclerView
 import dev.duprat.legacytraining.beer.model.Beer
 
 class BeerAdapter(private val beers: List<Beer>) : RecyclerView.Adapter<BeerViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BeerViewHolder =
-        BeerViewHolder(BeerCard(parent.context))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BeerViewHolder {
+        val beerCard = BeerCard(parent.context).apply {
+            layoutParams = RecyclerView.LayoutParams(
+                RecyclerView.LayoutParams.MATCH_PARENT,
+                RecyclerView.LayoutParams.WRAP_CONTENT
+            )
+        }
+
+        return BeerViewHolder(beerCard)
+    }
 
     override fun onBindViewHolder(holder: BeerViewHolder, position: Int) {
         holder.setBackgroundByPosition(position)
