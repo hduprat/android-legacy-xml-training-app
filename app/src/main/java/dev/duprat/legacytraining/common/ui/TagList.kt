6c790de7adb2e5877b7c34ff518c98fcf3279c00
hduprat <hugod@bam.tech>
2024-01-31 11:21:27 +0100

    feat(TagList): limit visible tags to 5

--- app/src/main/java/dev/duprat/legacytraining/common/ui/TagList.kt
+++ app/src/main/java/dev/duprat/legacytraining/common/ui/TagList.kt
@@ -5,11 +5,14 @@ import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.flexbox.FlexboxLayout
 import dev.duprat.legacytraining.R
 import dev.duprat.legacytraining.databinding.TagLayoutBinding
 import dev.duprat.legacytraining.databinding.TagListLayoutBinding
+
+const val MAX_TAGS = 5
 
 class TagList(context: Context, attrs: AttributeSet) : FlexboxLayout(context, attrs) {
     private var tagColor: Int
@@ -28,19 +31,30 @@ class TagList(context: Context, attrs: AttributeSet) : FlexboxLayout(context, at
     fun setTags(tags: List<String>) {
         binding.tagList.removeAllViews()
         val tagsSet = tags.toSet()
-        tagsSet.forEach { tag ->
-            val tagView = TagLayoutBinding.inflate(LayoutInflater.from(context), this, false).root
-            tagView.text = tag
-
-            val background = ContextCompat.getDrawable(context, R.drawable.tag_background)
-                ?.mutate() as GradientDrawable
-            background.setColor(tagColor)
-            tagView.background = background
-
-            val layoutParams = tagView.layoutParams as LayoutParams
-            layoutParams.marginEnd = 8
-
+        tagsSet.take(MAX_TAGS).forEach { tag ->
+            val tagView = generateTagView(tag)
+            binding.tagList.addView(tagView)
+        }
+        if (tagsSet.size > MAX_TAGS) {
+            val tagView = generateTagView("+${tagsSet.size - MAX_TAGS}")
             binding.tagList.addView(tagView)
         }
     }
+
+    private fun generateTagView(tag: String): TextView {
+        val tagView = TagLayoutBinding.inflate(LayoutInflater.from(context), this, false).root
+        tagView.text = tag
+
+        val background = ContextCompat.getDrawable(context, R.drawable.tag_background)
+            ?.mutate() as GradientDrawable
+        background.setColor(tagColor)
+        tagView.background = background
+
+        val layoutParams = tagView.layoutParams as LayoutParams
+        layoutParams.marginEnd = 8
+
+        return tagView
+    }
+
+
 }
