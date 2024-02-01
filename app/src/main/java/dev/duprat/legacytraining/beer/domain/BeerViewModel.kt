853c8435ef03d5fb0ac61f44d984879affc3abb8
hduprat <hugod@bam.tech>
2024-02-01 10:49:39 +0100

    fix(BeerViewModel): return state flow

--- app/src/main/java/dev/duprat/legacytraining/beer/domain/BeerViewModel.kt
+++ app/src/main/java/dev/duprat/legacytraining/beer/domain/BeerViewModel.kt
@@ -1,7 +1,17 @@
 package dev.duprat.legacytraining.beer.domain
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import org.mobilenativefoundation.store.store5.StoreReadResponse
+import org.mobilenativefoundation.store.store5.StoreReadResponseOrigin
 
-class BeerViewModel(repository: BeerRepository): ViewModel() {
+class BeerViewModel(repository: BeerRepository) : ViewModel() {
     val state = repository.getBeers()
+        .stateIn(
+            viewModelScope,
+            SharingStarted.Eagerly,
+            StoreReadResponse.Loading(StoreReadResponseOrigin.Fetcher())
+        )
 }
