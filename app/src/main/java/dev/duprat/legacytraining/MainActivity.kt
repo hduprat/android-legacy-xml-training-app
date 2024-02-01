3ac60a51a5990a63c2e9eae84f21524f4d88f1d7
hduprat <hugod@bam.tech>
2024-02-01 11:24:38 +0100

    feat(MainApplication): start Koin there

--- app/src/main/java/dev/duprat/legacytraining/MainActivity.kt
+++ app/src/main/java/dev/duprat/legacytraining/MainActivity.kt
@@ -3,22 +3,11 @@ package dev.duprat.legacytraining
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import dev.duprat.legacytraining.beer.domain.beerModule
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        startKoin {
-            modules(beerModule)
-        }
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onDestroy() {
-        stopKoin()
-        super.onDestroy()
     }
 }
